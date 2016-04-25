23b8639ffedc08a5cfca9afe37c5fa492c2ca730
chungvodim <ba198647@gmail.com>
2016-04-25 12:04:26 +0800

    Init

---
+++ HackerRank/MyString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public static class MyString
+    {
+        public static int Anagram(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return -1;
+            int anagram = 0;
+            int length = str.Length;
+            if (length > 10000) return -1;
+            if (length % 2 == 1) return -1;
+            int[] arr = new int[256];
+            for (int i = 0; i < length/2; i++)
+            {
+                arr[(int)str[i]]++;
+            }
+            for (int i = length / 2; i < length; i++)
+            {
+                arr[(int)str[i]]--;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                anagram += Math.Abs(arr[i]);
+            }
+            return anagram/2;
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt16(Console.ReadLine());
+            while (t > 0)
+            {
+                //int READLINE_BUFFER_SIZE = 10000;
+                //Stream inputStream = Console.OpenStandardInput(READLINE_BUFFER_SIZE);
+                //byte[] bytes = new byte[READLINE_BUFFER_SIZE];
+                //int outputLength = inputStream.Read(bytes, 0, READLINE_BUFFER_SIZE);
+                //Console.WriteLine(outputLength);
+                //char[] chars = Encoding.UTF8.GetChars(bytes, 0, outputLength);
+                var str = String.Join("",Console.ReadLine().Split());
+                //var str = new string(chars);
+                //str = str.Remove(str.Length - 1, 1);
+                //Console.WriteLine(new string(chars));
+                var result = MyString.Anagram(str);
+                Console.WriteLine(result);
+                t--;
+            }
+        }
+    }
+}
